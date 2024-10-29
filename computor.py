f02d8fffc6cdc38338712ff0b6e263738c1c48a9
Jamie <pomprab.b@gmail.com>
2024-10-29 01:41:44 +0100

    fixed higher degree cases

--- computor.py
+++ computor.py
@@ -24,15 +24,6 @@ def ft_sqrt(n, EPS=1E-10):
     return x
 
 
-def getDegree(a, b, c):
-    if a == 0 and b == 0:
-        return 0
-    elif a == 0:
-        return 1
-    else:
-        return 2
-
-
 def discriminant(a, b, c):
     return b * b - 4 * a * c
 
@@ -44,7 +35,7 @@ def format_solution(value):
 
 def constant_solution(c):
     if c == 0:
-        print("The equation has any real solution")
+        print("The equation has any real number solution")
     else:
         print("The equation has no solution")
     return
@@ -52,7 +43,7 @@ def constant_solution(c):
 
 def linear_solution(b, c):
     solution = format_solution(-c / b)
-    print("The solution is: {}".format(solution))
+    print(f"The solution is:\n{solution}")
     return
 
 
@@ -67,7 +58,7 @@ def quadratic_solution(a, b, c):
             print(format_solution(real + imaginary))
             print(format_solution(real - imaginary))
         else:
-            print("Discriminant less than zero, the two complex solutions are:")
+            print("Discriminant is strictly negative, the two complex solutions are:")
             print("{0} + {1} * i".format(format_solution(real), format_solution(imaginary)))
             print("{0} - {1} * i".format(format_solution(real), format_solution(imaginary)))
     else:
@@ -76,42 +67,22 @@ def quadratic_solution(a, b, c):
     return
 
 
-def parseCoefficients(part, parts):
-    a = 0.0
-    b = 0.0
-    c = 0.0
-
-    for elem in part:
-        if elem == '0' or elem == '':
-            continue
-
-        if "*" not in elem:
-            sys.exit("Error")
-
-        coefficient, var = elem.split('*')
-        var, degree = var.split('^')
-
-        # print(f"parts: {parts}")
-        # print(f"coefficient: {coefficient}\nvar: {var}\ndegree: {degree}")
-        if var != 'X' and var != 'x':
-            raise InputError('The variable can only be X or x')
-
-        try:
-            if int(degree) == 2:
-                a += float(coefficient)
-            elif int(degree) == 1:
-                b += float(coefficient)
-            elif int(degree) == 0:
-                c += float(coefficient)
-            else:
-                if elem not in parts[1]:
-                    print("\nPolynomial degree: {0}".format(int(degree)))
-                    sys.exit("The polynomial degree is strictly greater than 2, I can't solve.")
-
-        except ValueError:
-            raise InputError('Incorrect degree. Must be greater then 0')
-
-    return a, b, c
+def getCoefficientByDegree(reduced_coefficient):
+    a = 0
+    b = 0
+    c = 0
+    degree = 0
+    for deg in reduced_coefficient:
+        if reduced_coefficient[deg] != 0:
+            if degree < deg:
+                degree = deg
+            if deg == 2:
+                a = reduced_coefficient[deg]
+            elif deg == 1:
+                b = reduced_coefficient[deg]
+            elif deg == 0:
+                c = reduced_coefficient[deg]
+    return a, b, c, degree
 
 
 def parseEquation(equation):
@@ -128,48 +99,72 @@ def parseEquation(equation):
     return parts
 
 
+def parseCoefficients(part):
+    coefficients_dict = {}
+    counted_degrees = []
+
+    for elem in part:
+        # print(f"\npart: {part}\nelem: {elem}")
+        if (elem[0] == '0' and len(elem) > 1) or elem == '':
+            continue
+        if "*" not in elem:
+            sys.exit("Error")
+
+        coefficient, var = elem.split('*')
+        var, degree = var.split('^')
+
+        # print(f"\ncoefficient: {coefficient}\nvar: {var}\ndegree: {degree}")
+        if var != 'X' and var != 'x':
+            raise InputError('The variable can only be X or x')
+
+        try:
+            degree = int(degree)
+            if degree not in counted_degrees:
+                coefficients_dict[degree] = 0.0
+                counted_degrees.append(degree)
+            coefficient = float(coefficient)
+            coefficients_dict[degree] += coefficient
+
+        except ValueError:
+            raise InputError('Incorrect degree. Must be greater then 0')
+
+    return coefficients_dict
+
+
 def calculateCoefficients(equation):
     parts = parseEquation(equation)
-    a = 0
-    b = 0
-    c = 0
+    reduced_coefficients = {}
 
-    # print(f"parts: {parts}")
+    # print(f"\nparts: {parts}")
     for index, part in enumerate(parts):
-        a_tmp, b_tmp, c_tmp = parseCoefficients(part, parts)
-
-        # index = 0 means left side of the equation
-        # index = 1 means right side of the equation
         if index == 0:
-            a += a_tmp
-            b += b_tmp
-            c += c_tmp
+            left_coefficients = dict(sorted(parseCoefficients(part).items(), reverse=True))
         else:
-            a -= a_tmp
-            b -= b_tmp
-            c -= c_tmp
-    return a, b, c
+            right_coefficients = dict(sorted(parseCoefficients(part).items(), reverse=True))
+
+    degree_union = set(left_coefficients.keys()).union(set(right_coefficients.keys()))
+    # print(f"\nUnion: {degree_union}")
+
+    # for each degree, get either the value at that degree or 0 if it doesn't exist
+    for degree in degree_union:
+        left_value = left_coefficients.get(degree, 0)
+        right_value = right_coefficients.get(degree, 0)
+        reduced_coefficients[degree] = left_value - right_value
+
+    return dict(sorted(reduced_coefficients.items(), reverse=True))
 
 
-def display_information(a, b, c, degree):
-    print("\nPolynomial degree: {}".format(degree))
-    if (a == 0 and b == 0 and c == 0):
-        print("Reduced form: 0 = 0")
-    elif (a == 0 and b == 0):
-        print("Reduced form: {0} * X^0 = 0".format(c))
-    elif (a == 0 and c == 0):
-        print("Reduced form: {0} * X^1 = 0".format(b))
-    elif (b == 0 and c == 0):
-        print("Reduced form: {0} * X^2 = 0".format(a))
-    elif (a == 0):
-        print("Reduced form: {0} * X^1 + {1} * X^0 = 0".format(b, c))
-    elif (b == 0):
-        print("Reduced form: {0} * X^2 + {1} * X^0 = 0".format(a, c))
-    elif (c == 0):
-        print("Reduced form: {0} * X^2 + {1} * X^1 = 0".format(a, b))
-    else:
-        print("Reduced form: {0} * X^2 + {1} * X^1 + {2} * X^0 = 0".format(a, b, c))
-    return
+def display_reduced(reduced_coefficient):
+    terms = []
+    for degree in reduced_coefficient:
+        coefficient = reduced_coefficient[degree]
+        if coefficient == 0 and len(reduced_coefficient) > 1:
+            continue
+        else:
+            terms.append(f"{coefficient} * X^{degree}")
+    
+    reduced_form = " + ".join(terms).replace("+ -", "- ")
+    print(f"\nReduced form: {reduced_form} = 0")
 
 
 def display_solution(a, b, c, degree):
@@ -183,12 +178,18 @@ def display_solution(a, b, c, degree):
 
 
 def solve(equation):
-    a, b, c = calculateCoefficients(equation)
-    degree = getDegree(a, b, c)
+    reduced_coefficient = calculateCoefficients(equation)
+    # print("\nReduced coefficients: {0}".format(reduced_coefficient))
 
-    display_information(a, b, c, degree)
-    display_solution(a, b, c, degree)
-    return a, b, c
+    a, b, c, degree = getCoefficientByDegree(reduced_coefficient)
+    # print("degree: {0}".format(degree))
+
+    display_reduced(reduced_coefficient)
+    print(f"Polynomial degree: {degree}")
+    if degree > 2:
+        print("The polynomial degree is stricly greater than 2, I can't solve.")
+    else:
+        display_solution(a, b, c, degree)
 
 
 def main():
@@ -199,8 +200,8 @@ def main():
         print("\nExiting...")
     except EOFError:
         print("\nExiting...")
-    except InputError as i:
-        print(f"Error {i}")
+    except InputError:
+        print("Error")
 
 
 if __name__ == "__main__":
